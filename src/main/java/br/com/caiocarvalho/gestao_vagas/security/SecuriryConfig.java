c8f0100a2ad09a3c43861910359bdc93186dce13
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2024-11-27 22:21:44 -0400

    added swagger configs

--- src/main/java/br/com/caiocarvalho/gestao_vagas/security/SecuriryConfig.java
+++ src/main/java/br/com/caiocarvalho/gestao_vagas/security/SecuriryConfig.java
@@ -19,6 +19,12 @@ public class SecuriryConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-resouces/**"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -26,7 +32,8 @@ public class SecuriryConfig {
             auth.requestMatchers("/candidate/").permitAll()
                 .requestMatchers("/company/").permitAll()
                 .requestMatchers("/company/auth").permitAll()
-                .requestMatchers("/candidate/auth").permitAll();
+                .requestMatchers("/candidate/auth").permitAll()
+                .requestMatchers(SWAGGER_LIST).permitAll();
             auth.anyRequest().authenticated();
         })
         .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
