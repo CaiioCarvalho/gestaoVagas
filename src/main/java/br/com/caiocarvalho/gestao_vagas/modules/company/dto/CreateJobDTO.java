74fd946176a96d24d50607102ee731b7290411fb
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2024-11-30 16:39:48 -0400

    create job dto refactoring

--- src/main/java/br/com/caiocarvalho/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/caiocarvalho/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,10 +1,18 @@
 package br.com.caiocarvalho.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+
+    @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "GymPass, plano de saúde", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "Júnior", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
