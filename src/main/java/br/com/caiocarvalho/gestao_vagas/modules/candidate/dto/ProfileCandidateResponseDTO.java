29e2e3ea1596dd87ac638f3a378e4918b30f80b0
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2024-11-30 16:58:44 -0400

    adjusting all the swagger configs

--- src/main/java/br/com/caiocarvalho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/caiocarvalho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.caiocarvalho.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
 
+    @Schema(example = "Desenvolvedora Java")
     private String description;
+
+    @Schema(example = "mariajulia")
     private String username;
+
+    @Schema(example = "juliam@gmail.com")
     private String email;
     private UUID id;
+
+    @Schema(example = "Maria Julia Silva")
     private String name;
 }
